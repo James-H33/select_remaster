55e0ed3f85beddf0c626a57cda367f5b73e22d9e
James Hall <llahwj33@gmail.com>
2016-06-21 13:52:00 -0400

    Added File Compression, Lots of images and changed a few styles.

--- seed.js
+++ seed.js
@@ -7,11 +7,11 @@ var data = [
     nameExt: 'PORSCHE 356 PRÉ-A 1600 SPEEDSTER',
     year: '1954',
     images: [
-    'http://www.hessclassic.com/app/uploads/2016/03/hessclassic-PORSCHE_356.png?82f50c',
-    'http://www.hessclassic.com/app/uploads/2016/04/tmp-porsche-bg-3.jpg',
-    'http://www.hessclassic.com/app/uploads/2016/04/tmp-porsche-6.jpg?82f50c',
-    'http://www.hessclassic.com/app/uploads/2016/04/tmp-porsche-bg-4.jpg',
-    'http://www.hessclassic.com/app/uploads/2016/04/tmp-porsche-5.jpg?82f50c'
+    '/imgs/tmp-porsche.png',
+    '/imgs/tmp-porsche-bg-3.jpg',
+    '/imgs/tmp-porsche-6.jpg',
+    '/imgs/tmp-porsche-bg-4.jpg',
+    '/imgs/tmp-porsche-5.jpg'
    ],
     info: [
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer.',
@@ -22,11 +22,11 @@ var data = [
     histImage: ['http://1.bp.blogspot.com/-dP7fOD9ZDXg/UJuamwLA1KI/AAAAAAAAS3I/Ir7c2pAnV8A/s1600/James-Dean-Porsche-356-Speedster.jpg'],
   },
   {
-    name: 'FORD THUNDERBIRD',
-    nameExt: ' FORD THUNDERBIRD CV',
-    year: '1957',
+    name: 'Porsche 911',
+    nameExt: ' Porche 911 Turbo Coupe',
+    year: '1965',
     images: [
-    '/imgs/1957-ford-thunderbird.png',
+    '/imgs/p911White-1.png',
     '/imgs/p911White-2.jpg',
     '/imgs/p911White-3.jpg',
     '/imgs/p911White-4.jpg',
@@ -41,11 +41,11 @@ var data = [
     histImage: ['/imgs/p911White-hist.jpg'],
   },
   {
-    name: 'PORSCHE TYPE C',
-    nameExt: 'PORSCHE AUTO UNION TYPE C',
-    year: '1936',
+    name: 'Corvette Coupe',
+    nameExt: 'Corvette Coupe 427',
+    year: '1965',
     images: [
-    '/imgs/auto-1.png',
+    '/imgs/red-corv_01.png',
     '/imgs/p911White-2.jpg',
     '/imgs/p911White-3.jpg',
     '/imgs/p911White-4.jpg',
